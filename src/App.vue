06a9aedaaa73f14eda9a8cef079b7fbcaabd4a66
mrrtun007 <68525887+mrrtun007@users.noreply.github.com>
2020-09-29 13:40:01 +0700

    11

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+ <div id="app">
+ 
+    <HelloWorld msg="Rx-7" />
+    <hr>
+    <div class="row">
+      <div class="col-6">
+   <img src="https://www.drifted.com/wp-content/uploads/2014/02/MadMike_1920x1280.jpg" width="500" height="300" /> 
+     
+     
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+  <img src="https://i.pinimg.com/originals/11/39/38/1139388fb46dbbf1663492857efec811.jpg" width="350" height="300" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sem" />
+      </div>
+    </div>
+    
+  </div>
+</template>
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CsButton.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    CsButton,
+    
+  },
+  data : function(){
+    return {
+    total1: 0,
+    total2: 0,
+   
+   } 
+  },
+  methods: {
+  sum(counterValue){
+    this.total1 = counterValue
+  },
+  
+  sem(counterValue){
+    this.total2 = counterValue
+ 
+  
+},
+},
+
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ff0bde;
+  margin-top: 60px;
+  
+}
+body {
+
+  background-size: 100%;
+}
+</style>
+
---
+++ src/components/CsButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-dark">ถูกใจมาก</button>
+    <button v-on:click="downFunction" class="btn btn-dark ml-3">ไม่ถูกใจอย่างแรง</button>
+    <button v-on:click="LoveFunction" class="btn btn-dark ml-3">รักมากมาย</button>
+    <button v-on:click="ResetFunction" class="btn btn-dark ml-3">รีเซ็ท</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 1;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
